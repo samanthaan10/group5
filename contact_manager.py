c56b1ac4fb13c268185feb38a139c4c190abefbf
samanthaan10 <157721984+samanthaan10@users.noreply.github.com>
2024-04-09 17:47:26 -0400

    Update contact_manager.py with contact docstring

--- contact_manager.py
+++ contact_manager.py
@@ -3,3 +3,11 @@ print("hi")
 
 print("hello world")
 print ("project")
+
+"""A class for individual contacts
+
+  Attributes: 
+        name (str): The name of the contact
+        phone_number (str): The phone number for the contact
+        email (str): The email address for the contact
+"""
