c59ae5ec04ef3864bf08e4ec83aa5713cbca2192
jessiebarke <145034444+jessiebarke@users.noreply.github.com>
2024-04-09 19:11:32 -0400

    Update contact_manager.py

--- contact_manager.py
+++ contact_manager.py
@@ -11,3 +11,21 @@ print ("project")
         phone_number (str): The phone number for the contact
         email (str): The email address for the contact
 """
+
+
+
+def main ():
+  """Takes the username and password of person logging in and displays address book if username and password are correct
+  Parameters:
+    username (str)
+      username of the person logging in
+    password (str)
+      password that matches with the username 
+
+  Returns:
+      - if username and password are correct return:
+            address_book
+              completely organized/managed address book of the user
+      - else return:
+        None
+  """
