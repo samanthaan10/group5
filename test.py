f4698016806c99e497183fcd1f15e43bdf510be0
samanthaan10 <157721984+samanthaan10@users.noreply.github.com>
2024-04-23 14:01:32 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
 #Unit Tests for Contact Manager
 
+    def test_add_contact(self):
+        contact_name = "Rob"
+        contact_phone = "4430300210"
+        contact_email = "rob@gmail.com"
+        self.contact_manager.add_contact(contact_name, contact_phone, contact_email)
+
+        self.assertEqual(len(self.contact_manager.contacts), 1)
+        added_contact = self.contact_manager.contacts[0]
+        self.assertEqual(added_contact.name, contact_name)
+        self.assertEqual(added_contact.phone_number, contact_phone)
+        self.assertEqual(added_contact.email, contact_email)
+
+
     def test_delete_contact_existing(self): 
         self.user = User("Jen")
         self.user.add_contact("John", "7814676223", "john@gmail.com")
